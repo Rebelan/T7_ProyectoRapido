e16789e5462b6082b0cecd2512f5b185c13fd4f7
Rebelan <abelcm2013@gmail.com>
2025-03-24 20:00:03 +0100

    Pulir menu

--- Main.java
+++ Main.java
@@ -1,57 +1,63 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        boolean salir = false;
+        while (!salir) {
+            int op = Integer.parseInt(sc.nextLine());
+            switch (op) {
+                case 1:
 
-        System.out.println("1. Seleccionar una carpeta");
-        System.out.println("2. Leer un fichero");
-        System.out.println("3. Convertir a ...");
-        System.out.println("4. Salir");
-        System.out.print("Elige una opción: ");
-        int a = sc.nextInt();
+                    break;
+                case 2:
 
-        switch (a) {
-            case 1:
+                    break;
+                case 3:
+                    System.out.println("1. .csv");
+                    System.out.println("2. .json");
+                    System.out.println("3. .xml");
+                    System.out.print("¿A qué extensión quieres convertir?: ");
+                    int b = sc.nextInt();
+                    switch (b) {
+                        case 1:
 
-                break;
+                            break;
 
-            case 2:
+                        case 2:
 
-                break;
-            case 3:
-                System.out.println("1. .csv");
-                System.out.println("2. .json");
-                System.out.println("3. .xml");
-                System.out.print("¿A qué extensión quieres convertir?: ");
-                int b = sc.nextInt();
-                switch (b) {
-                    case 1:
+                            break;
 
-                        break;
+                        case 3:
 
-                    case 2:
+                            break;
 
-                        break;
-                        
-                    case 3:
+                        default:
+                            break;
+                    }
 
-                        break;
+                    break;
 
-                    default:
-                        break;
-                }
+                case 4:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+                    break;
+            }
 
-                break;
-
-            case 4:
-
-                break;
-
-            default:
-                break;
+            sc.close();
         }
 
-        sc.close();
+    }
+
+    public static void menu() {
+        System.out.println("1. Seleccionar una carpeta");
+        System.out.println("2. Leer un fichero");
+        System.out.println("3. Convertir op ...");
+        System.out.println("4. Salir");
+        System.out.print("Elige una opción: ");
     }
 }

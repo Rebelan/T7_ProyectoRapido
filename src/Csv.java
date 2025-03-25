f927eb61a2f0bce83da16b96f8f34933b69bac14
Rebelan <abelcm2013@gmail.com>
2025-03-25 17:29:40 +0100

    commit

--- src/Csv.java
+++ src/Csv.java
@@ -1,36 +1,31 @@
 package src;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Csv {
-    
-    public static ArrayList<HashMap<String, Integer>> LecturaFicheroCSV(File ficheroCSV, File salida){
 
-        HashMap <String, Integer> infoFichero = new HashMap<String, Integer>();
+    public static ArrayList<HashMap<String, Integer>> LecturaFicheroCSV(File ficheroCSV) {
+
         ArrayList<HashMap<String, Integer>> listaInfoFichero = new ArrayList<HashMap<String, Integer>>();
-        try (BufferedReader br = new BufferedReader(new FileReader(ficheroCSV));
-             BufferedWriter bw = new BufferedWriter(new FileWriter(salida))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(ficheroCSV))) {
             String linea;
             while ((linea = br.readLine()) != null) {
+                HashMap<String, Integer> infoFichero = new HashMap<String, Integer>();
                 String[] palabras = linea.split(",");
                 for (int i = 0; i < palabras.length; i++) {
                     infoFichero.put(palabras[i], i);
                 }
                 listaInfoFichero.add(infoFichero);
-                bw.write(infoFichero.toString());
+
             }
-            
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
         return listaInfoFichero;
     }
 }

66d6658f2ffdb449df2519e5cd1812a30df0236c
Rebelan <abelcm2013@gmail.com>
2025-03-25 17:16:03 +0100

    Maldito xml de mierda

--- src/Xml.java
+++ src/Xml.java
@@ -1,28 +1,34 @@
 package src;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.lang.classfile.BufWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Xml {
-    
-    public static ArrayList<HashMap<String, Integer>> LecturaFicheroXML(File fileXML, File salida){
-        
-        try(BufferedReader br = new BufferedReader(new FileReader(fileXML));
-            BufferedWriter bw = new BufferedWriter(new FileWriter(salida))){
+
+    public static ArrayList<HashMap<String, Integer>> LecturaFicheroXML(File fileXML, File salida) {
+        ArrayList<HashMap<String, Integer>> listaInfoFichero = new ArrayList<HashMap<String, Integer>>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileXML))) {
             String linea;
-            while ((linea = br.readLine()) != null){
-                String[] palabras = linea.split(" ");
-                
+            linea = br.readLine();
+            linea = br.readLine();
+            String objeto = linea;
+            while ((linea = br.readLine()) != null) {
+                if (linea.equals(objeto)) {
+                    HashMap<String, Integer> infoFichero = new HashMap<String, Integer>();
+                    String[] palabras = linea.split("<[^>]+>");
+                    for (int i = 0; i < palabras.length; i++) {
+                        infoFichero.put(palabras[i], i);
+                    }
+                    listaInfoFichero.add(infoFichero);
+                }
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Error al leer el fichero");
         }
-        
-        return null;
+
+        return listaInfoFichero;
     }
 }
